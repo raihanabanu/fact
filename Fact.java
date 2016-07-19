6c88c0aae6b3fa694db8ac9d822b7f8f363e9edf
raihanabanu <raihanabanu.s@gmail.com>
2016-07-19 10:52:24 +0530

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,19 @@
+# fact
+class Fact{
+public static void main(String args[0])
+{
+int f=1;
+int count=5;
+int i=integer.parseInt(args[0]);
+for(i=0;i<=count;i++)
+{
+f=f*i;
+System.out.println(the factorial of"+i+"is"+f);
+}
+if(i==0)
+{
+f=f+i;
+System.out.println(the factorial of"+i+"is"+f);
+}
+}
+}
